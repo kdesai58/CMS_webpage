4db94ad3370d00ccbf3fa331ca83b16babbc3dc7
kdesai58 <kdesai58@uwo.ca>
2024-11-26 13:45:33 -0500

    changes for the summeries

--- app/routes/summary.py
+++ app/routes/summary.py
@@ -1,7 +1,12 @@
+import hashlib
 from fastapi import APIRouter, UploadFile, File
 from typing import List
 
+from app.db import is_file_duplicate, save_file_metadata
+from app.services.embeddings import generate_embeddings
+from app.services.faiss_search import add_to_index
 from app.services.summarizer import generate_summary
+from app.services.text_extraction import extract_text_from_pdf
 
 router = APIRouter()
 
@@ -9,8 +14,27 @@ router = APIRouter()
 async def summarize_pdf(files: List[UploadFile] = File(...)):
     summaries = []
     for file in files:
-        content = await file.read()
-        summary = f"Summary for {file.filename}: {len(content)} bytes processed."
-        summaries.append({"filename": file.filename, "summary": summary})
+        file_path = f"uploads/{file.filename}"
+        file_content = await file.read()
+
+        file_hash = hashlib.sha256(file_content).hexdigest()  # Generate hash
+        await file.seek(0)  # Reset the file pointer so the content can be re-read
+
+        with open(file_path, "wb") as f:
+            f.write(file_content)
+
+        if is_file_duplicate(file_hash):
+            text = extract_text_from_pdf(file_path)
+            summary = generate_summary(text)
+            summaries.append({"filename": file.filename, "Status": f"{file.filename} is already in the database." , "summary": summary})
+        else:
+
+            text = extract_text_from_pdf(file_path)
+            summary = generate_summary(text)
+            embedding = generate_embeddings(text)
+            doc_id = save_file_metadata(file.filename, text, file_hash)
+            add_to_index(doc_id, embedding)
+            summaries.append({"filename": file.filename, "Status": f"{file.filename} is proccessed and added to the database." , "summary": summary})
+            
 
     return {"summaries": summaries}
