ad5a6b00276d94f332b888b89b5cff3ef94ff31b
kdesai58 <kdesai58@uwo.ca>
2024-11-25 19:25:45 -0500

    user can upload multiple file

--- my-cms-frontend/src/components/UploadPage.js
+++ my-cms-frontend/src/components/UploadPage.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 
 function UploadPage() {
-    const [file, setFile] = useState(null);
+    const [files, setFiles] = useState([]);
 
     const handleUpload = async () => {
         const formData = new FormData();
-        formData.append("file", file);
+
+        Array.from(files).forEach((file) => {
+            formData.append("files", file); // Use the key "files" for all files
+        });
 
         const response = await fetch("http://localhost:8000/upload/", {
             method: "POST",
@@ -19,7 +22,10 @@ function UploadPage() {
     return (
         <div>
             <h2>Upload Document</h2>
-            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+            <input
+                type="file" 
+                multiple 
+                onChange={(e) => setFiles(e.target.files)} />
             <button onClick={handleUpload}>Upload</button>
         </div>
     );
