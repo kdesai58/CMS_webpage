f37dec32edf56bcaa3713341f44cde93cd7f4b66
Parth Valand <pvaland@uwo.ca>
2024-11-26 12:37:00 -0500

    Added color and heading to webpage in App.js

--- my-cms-frontend/src/App.js
+++ my-cms-frontend/src/App.js
@@ -6,6 +6,9 @@ function App() {
     return (
         <div>
             <h1>Document Management System</h1>
+            <b>
+                <p>Hey there! To get started, upload a file and then choose what to do with it...</p>
+            </b>
             <UploadPage />
             <SearchPage />
         </div>
