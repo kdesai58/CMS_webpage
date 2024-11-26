b4dfc98f0f33a2aa7bb6c2bf9e6546f3bc1b2172
kdesai58 <kdesai58@uwo.ca>
2024-11-25 20:59:49 -0500

    updated summarizer

--- app/routes/summary.py
+++ app/routes/summary.py
@@ -1,6 +1,12 @@
-from fastapi import APIRouter, UploadFile
+import hashlib
+from typing import List
+from fastapi import APIRouter, File, UploadFile
 
+from app.db import is_file_duplicate, save_file_metadata
+from app.services.embeddings import generate_embeddings
+from app.services.faiss_search import add_to_index
 from app.services.summarizer import generate_summary
+from app.services.text_extraction import extract_text_from_pdf
 
 router = APIRouter()
 
@@ -8,6 +14,34 @@ router = APIRouter()
 async def summarize_pdf(files: List[UploadFile] = File(...)):
     responce = []
     for file in files:
-        summary = generate_summary(text)
+        file_path = f"uploads/{file.filename}"
+        file_content = await file.read()
+
+        file_hash = hashlib.sha256(file_content).hexdigest()  # Generate hash
+        await file.seek(0)  # Reset the file pointer so the content can be re-read
+
+        with open(file_path, "wb") as f:
+            f.write(file_content)
+
+        if is_file_duplicate(file_hash):
+            text = extract_text_from_pdf(file_path)
+            summary = generate_summary(text)
+            responce.append({
+                "filename": file.filename,
+                "message": "File already exists in the database",
+                "summary": summary
+            })
+        else:
+            text = extract_text_from_pdf(file_path)
+            embedding = generate_embeddings(text)
+            # return {'embedding_id': embedding_id}
+            doc_id = save_file_metadata(file.filename, text, file_hash)
+            add_to_index(doc_id, embedding)
+            summary = generate_summary(text)
+            responce.append({
+                "filename": file.filename,
+                "message": "File uploaded and processed",
+                "summary": summary
+            })
 
     return {"summaries of files": responce}
