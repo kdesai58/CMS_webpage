2efec8841f1a891ca110b26d7f7c9ec1bbd6d8bc
kdesai58 <kdesai58@uwo.ca>
2024-11-27 12:55:21 -0500

    updated things

--- app/services/summarizer.py
+++ app/services/summarizer.py
@@ -6,4 +6,6 @@ from transformers import pipeline
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 def generate_summary(text: str):
+    if(len(text) > 512):
+        return "Text is too long to summarize. please provide a pdf with less than 512 words"
     return summarizer(text, max_length=200, min_length=50, do_sample=False)[0]['summary_text']
